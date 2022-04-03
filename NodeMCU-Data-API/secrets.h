a5fa612c50e63d6246380c6931c6b79fce948659
EdSchenk <e.schenk@ma-web.nl>
2022-04-04 01:14:25 +0200

    init

---
+++ NodeMCU-Data-API/secrets.h
@@ -0,0 +1,6 @@
+#define WIFI_SSID "Brownies"
+#define WIFI_PASSWORD "ellabella!"
+#define SERVER_HOST "data.softwaredeveloper.amsterdam"
+#define SERVER_PORT 443
+#define DEVICE_KEY "12345" // gebruik jouw eigen key
+#define DEVICE_SECRET "54321" // gebruik jouw eigen secret
